1933141ced2e7b7881445d610affd54a20b66bff
Dipesh <shahdipesh97@gmail.com>
2023-01-02 20:41:25 +0545

    HelloWeb Project added

---
+++ HelloWeb/Controllers/NumberController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWeb.Controllers
+{
+    public class NumberController : Controller
+    {
+        public IActionResult Multiplication()
+        {
+            return View();
+        }
+
+        public IActionResult MultiplicationFrom(int value) 
+        {
+            return View(value);
+        }
+    }
+
+
+}
