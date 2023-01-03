8b052b3f372eb2f014ec9759cd7d6cadb627e6c9
Dipesh <shahdipesh97@gmail.com>
2023-01-03 21:30:46 +0545

    DropDown functionality added for Departments in Add Employee model

--- HRM/HRM.Web/Controllers/EmployeeController.cs
+++ HRM/HRM.Web/Controllers/EmployeeController.cs
@@ -1,57 +1,62 @@
 ﻿using HRM.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HRM.Web.Controllers
 {
-	public class EmployeeController : Controller
-	{
-		private HRDbContext db = new HRDbContext();
+    public class EmployeeController : Controller
+    {
+        private HRDbContext db = new HRDbContext();
 
-		public IActionResult Index()
-		{
-			var employees = db.Employees.ToList();
-			return View(employees);
-		}
+        public IActionResult Index()
+        {
+            var employees = db.Employees.ToList();
+            return View(employees);
+        }
 
-		public IActionResult Add()
-		{
-			return View();
-		}
+        public IActionResult Add()
+        {
+            var departments = db.Departments.Select(x => new SelectListItem { Text = x.Name, Value = x.Name }).ToList();
 
-		[HttpPost]
-		public IActionResult Add(Employee employee)
-		{
-			db.Employees.Add(employee);
-			db.SaveChanges();
-			return RedirectToAction(nameof(Index));
-		}
+            ViewData["departments"] = departments;
 
-		public IActionResult Edit(int id)
-		{
-			var employee = db.Employees.Find(id);
-			return View(employee);
-		}
+            return View();
+        }
 
-		[HttpPost]
-		public IActionResult edit(Employee employee)
-		{
-			db.Employees.Update(employee);
-			db.SaveChanges();
-			return RedirectToAction(nameof(Index));
-		}
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
 
-		public IActionResult Delete(int id)
-		{
-			var employee = db.Employees.Find(id);
-			return View(employee);
-		}
+        public IActionResult Edit(int id)
+        {
+            var employee = db.Employees.Find(id);
+            return View(employee);
+        }
 
-		[HttpPost]
-		public IActionResult Delete(Employee employee)
-		{
-			db.Employees.Remove(employee);
-			db.SaveChanges();
-			return RedirectToAction(nameof(Index));
-		}
-	}
+        [HttpPost]
+        public IActionResult edit(Employee employee)
+        {
+            db.Employees.Update(employee);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var employee = db.Employees.Find(id);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Employee employee)
+        {
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
--- HRM/HRM.Web/HRDbContext.cs
+++ HRM/HRM.Web/HRDbContext.cs
@@ -3,13 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HRM.Web
 {
-    public class HRDbContext : DbContext
-    {
-        public DbSet<Employee> Employees { get; set; }
+	public class HRDbContext : DbContext
+	{
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Department> Departments { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Data Source =(localdb)\\mssqllocaldb;Initial Catalog=HumanResource;Integrated Security=true");
-        }
-    }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source =(localdb)\\mssqllocaldb;Initial Catalog=HumanResource;Integrated Security=true");
+		}
+	}
 }
