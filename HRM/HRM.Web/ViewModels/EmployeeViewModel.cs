7daa0c7e63a007cfe78ee2334e46d1bf62aae1b8
Dipesh <shahdipesh97@gmail.com>
2023-01-11 05:15:35 +0545

    client side validation added

--- HRM/HRM.Web/ViewModels/EmployeeViewModel.cs
+++ HRM/HRM.Web/ViewModels/EmployeeViewModel.cs
@@ -1,28 +1,40 @@
 ﻿using HRM.Web.Enums;
 using HRM.Web.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace HRM.Web.ViewModels
 {
-	public class EmployeeViewModel
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public string? Address { get; set; }
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
 
-		public string Email { get; set; }
+        [Required(ErrorMessage = "We need your name")]
+        [MinLength(2, ErrorMessage = "Please enter atleat 2 letters")]
+        public string Name { get; set; }
 
-		public Gender Gender { get; set; }
+        public string? Address { get; set; }
 
-		public DateTime? Dob { get; set; }
-		public DateTime JoinDate { get; set; }
+        [Required(ErrorMessage = "We need your email")]
+        public string Email { get; set; }
 
-		public string Designation { get; set; }
+        public Gender Gender { get; set; }
 
-		public IFormFile ProfileImage { get; set; }
+        [Display(Name = "")]
+        public DateTime? Dob { get; set; }
 
-		public string? ProfileImagePath { get; set; }
+        [Display(Name = "Join Date")]
+        public DateTime JoinDate { get; set; }
 
-		public string? DepartmentName { get; set; }
-		public int DepartmentId { get; set; }
-	}
+        public string Designation { get; set; }
+
+        [Display(Name = "Upload Your Avatar")]
+        public IFormFile ProfileImage { get; set; }
+
+        public string? ProfileImagePath { get; set; }
+
+        [Display(Name = "Department")]
+        public string? DepartmentName { get; set; }
+
+        public int DepartmentId { get; set; }
+    }
 }
